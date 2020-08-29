3f385888ea93f6ae1000b329f07b53ab4034b01a
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-08-30 00:26:47 +0530

    Fixed a bug

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-var text=$("#text").val();
+let qrcode=new QRCode("qrcode");
 
+document.getElementById("generate").addEventListener("click", function() {
+    let text=document.getElementById("qrtext").value;
 
+    if( text != "") {
+        //new QRCode(document.getElementById("qrcode"), text);
+        qrcode.makeCode(text);
+    }
+})
 
-function qrwork() {
-	if(text!=$("#text").val()) {
-		$("#qrcode").qrcode(text);
-	}
-}
-
-function clr() {
-		text=''
-		$("#qrcode").html("");
-	
-}
-/*function _(e, all=false) {
-    let divs = document.querySelectorAll(e);
-    if (all || (divs.length > 1)) { return divs; }
-    return divs[0];
-}
-
-
-function download(){
-    let link = document.createElement('a');
-    let qr=_("#qrcode"); 
-
-    link.setAttribute('download', 'mandala.png');
-    link.setAttribute('href', qr.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-    link.click();
-}
-*/
+document.getElementById("reset").addEventListener("click", function() {
+    document.getElementById("qrcode").innerHTML="";
+    document.getElementById("qrtext").value="";
+})
